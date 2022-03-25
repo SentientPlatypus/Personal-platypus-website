5f7ef0c05d494333ce0521ea1bb2e6b9ea516fcc
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2022-03-24 22:04:04 -0400

    did the shit in the wrong dir

--- code/services/constants.py
+++
@@ -1,5 +0,0 @@
-EMAILREGEX            = '\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-EMAIL                 = "trexycrocs@gmail.com"
-EMAILPASSWORD         = "geneavianina"
-PORT                  = 465  # For SSL
-# Create a secure SSL context
