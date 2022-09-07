d31a670da382459973f9f0c7f9340726eb93f37f
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2022-09-07 19:37:00 -0400

    added sidebar. fixed cursors on subpages

---
+++ flaskApp/static/js/sidebarHandler.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "200px";
+    // document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    // document.getElementById("main").style.marginLeft= "0";
+  }
