0ec24c4f5744740ec30ea6a2fcc68cc9db2f3477
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2024-02-21 15:34:55 -0500

    fresh venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/mnt/d/Code/portfolio/personalwebsite/personal-platypus-website/venv"
+set -gx VIRTUAL_ENV "/mnt/d/Code/Portfolio/PersonalWebsite/Personal-platypus-website/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
